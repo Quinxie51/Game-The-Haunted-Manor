72d86da50864a78d44f0f8702b0a4bbe2e494350
Quinxie51 <114624411+Quinxie51@users.noreply.github.com>
2024-01-18 10:50:29 -0600

    add light follow cursor function to the living room

--- script/living_room.gd
+++ script/living_room.gd
@@ -1,7 +1,22 @@
 extends Node2D
-
+var light_node: Light2D
 var targetScenePath: String = "res://scene/bedroom.tscn"
+@export var follow_speed: float = 5.0  
 
 func _on_bedroom_door_input_event(viewport, event, shape_idx):
 	if event.is_pressed():
 		get_tree().change_scene_to_file(targetScenePath)
+
+
+
+func _ready():
+	light_node = $Light2D  
+
+func _process(delta):
+	# Get the global position of the mouse cursor
+	var target_position = get_global_mouse_position()
+	# Smoothly move the light node towards the mouse cursor using linear_interpolate
+	light_node.global_position = light_node.global_position.linear_interpolate(target_position, follow_speed * delta)
+	light_node.color = Color(1, 1, 1)  
+	light_node.radius = 200.0
+
--- script/main_game.gd
+++ script/main_game.gd
@@ -1,2 +1,5 @@
 extends Node2D
 
+func _ready():
+	OS.shell_open("https://www.youtube.com/watch?v=bllKLAiLo6g")
+
