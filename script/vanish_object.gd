10ff396bf5c82a97a61c5c6c30ea1f102efbb2ff
Quinxie51 <114624411+Quinxie51@users.noreply.github.com>
2024-01-18 21:37:17 -0600

    finish the kitchen corner hidden objects

--- script/vanish_object.gd
+++ script/vanish_object.gd
@@ -5,12 +5,12 @@ extends Area2D
 @onready var EFFECT = preload("res://scene/particles.tscn")
 
 func _on_portal_area_input_event(viewport, event, shape_idx):
-	if event.is_pressed():
+	if event.is_pressed() && vanishObject != null:
 		var effect = EFFECT.instantiate()
 		add_child(effect)
 		effect.global_position = event.global_position
 		vanishObject.queue_free()
 		if objects.get_child_count() == 0:
 			print("all  done")
-		queue_free() #remove the area
+		#queue_free() #remove the area
 
