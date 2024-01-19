5fb78b93369e809caed2c021307cde451ee2e964
Quinxie51 <114624411+Quinxie51@users.noreply.github.com>
2024-01-18 22:30:41 -0600

    check if all the object is found in kitchen

--- script/kitchen_corner.gd
+++ script/kitchen_corner.gd
@@ -1,3 +1,9 @@
 extends Node2D
 
+@onready var objects = $Objects
+var message_printed = false  # Flag to track whether the message has been printed
 
+func _physics_process(delta):
+	if objects.get_child_count() == 0 and not message_printed:
+		print("all done")
+		message_printed = true
--- script/vanish_object.gd
+++ script/vanish_object.gd
@@ -1,7 +1,6 @@
 extends Area2D
 
 @export var vanishObject : Label
-@onready var objects = $"../../Objects"
 @onready var EFFECT = preload("res://scene/particles.tscn")
 
 func _on_portal_area_input_event(viewport, event, shape_idx):
@@ -10,7 +9,5 @@ func _on_portal_area_input_event(viewport, event, shape_idx):
 		add_child(effect)
 		effect.global_position = event.global_position
 		vanishObject.queue_free()
-		if objects.get_child_count() == 0:
-			print("all  done")
-		#queue_free() #remove the area
+		
 
