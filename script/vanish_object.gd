d51ef5b3256e71626cf16cf39b841aa3c4d8d5fc
Quinxie51 <114624411+Quinxie51@users.noreply.github.com>
2024-01-24 19:49:52 -0600

    add a side game with tile map and character

--- script/vanish_object.gd
+++ script/vanish_object.gd
@@ -7,8 +7,8 @@ func _on_portal_area_input_event(viewport, event, shape_idx):
 	if event.is_pressed() && vanishObject != null:
 		var effect = EFFECT.instantiate()
 		add_child(effect)
-		effect.global_position = event.global_position
-		vanishObject.queue_free()
+		effect.global_position = event.position
+		print("effect at global pos: ", effect.global_position)
+		#vanishObject.queue_free()
 		
 		
-
