6f013bc498297c92263ec4811d4be510bfc0a9b3
Quinxie51 <114624411+Quinxie51@users.noreply.github.com>
2024-01-28 13:43:21 -0600

    connect puzzle with library

--- script/libraryDoor.gd
+++ script/libraryDoor.gd
@@ -5,11 +5,11 @@ var time = 0
 
 func _on_input_event(viewport, event, shape_idx):
    if event.is_pressed():
-      #var puzzle = ui_layer.get_puzzle()
-      #if !puzzle.board_game_won():
-      ui_layer.show_puzzle(true)
-      #elif puzzle.board_game_won():
-         #var targetScenePath = "res://scene/"+targetScene+".tscn"
-         #get_tree().change_scene_to_file(targetScenePath)
+      var puzzle = ui_layer.get_puzzle()
+      if !puzzle.puzzle_won():
+         ui_layer.show_puzzle(true)
+      elif puzzle.puzzle_won():
+         var targetScenePath = "res://scene/"+targetScene+".tscn"
+         get_tree().change_scene_to_file(targetScenePath)
          
 
--- script/puzzle.gd
+++ script/puzzle.gd
@@ -9,12 +9,13 @@ func _on_restart_pressed():
    board.scramble_board()
    board.game_state = board.GAME_STATES.STARTED
 
-
-func board_game_won():
+func _on_board_game_won():
    label.text = 'You win'
    game_won = true
    await get_tree().create_timer(1.5).timeout
    hide()
-   #var targetScenePath = "res://scene/"+targetScene+".tscn"
-   #get_tree().change_scene_to_file(targetScenePath)
+   var targetScenePath = "res://scene/"+targetScene+".tscn"
+   get_tree().change_scene_to_file(targetScenePath)
+func puzzle_won():
    return game_won
+
